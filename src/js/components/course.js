00e18615328dd7ce5857fa9609b8fa8ef33f07df
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2023-05-06 14:07:52 +0300

    course slider adaptive

--- src/js/components/course.js
+++ src/js/components/course.js
@@ -3,23 +3,45 @@ import Swiper, { Navigation, Pagination } from "swiper";
 function courseSlider() {
 	const swiper = new Swiper('.swiper', {
 		modules: [Navigation, Pagination],
+		direction: 'horizontal',
 		autoHeight: true,
-		loopPreventsSliding: true,
-		spaceBetween: 60,
-		centeredSlidesBounds: true,
-		slidesPerView: 3,
+		spaceBetween: 30,
+		loop: true,
+		slidesPerView: 1.2,
+		slidesPerGroup: 1,
+		initialSlide: 1,
+		centeredSlides: true,
+
 		// If we need pagination
 		pagination: {
 			el: '.swiper-pagination',
 			type: 'bullets',
 			clickable: true,
 		},
-
 		// Navigation arrows
 		navigation: {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
 		},
+
+		breakpoints: {
+			480: {
+				slidesPerView: 2,
+				centeredSlides: true,
+			},
+
+			480: {
+				loop: true,
+			},
+
+			1000: {
+				loop: false,
+				slidesPerView: 3,
+				slidesPerGroup: 1,
+				initialSlide: 0,
+				centeredSlides: false,
+			}
+		}
 	  });
 }
 
